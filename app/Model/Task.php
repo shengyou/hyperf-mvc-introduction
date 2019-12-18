efd98901b3c472b4520608a11e0ea36739bfe404
Shengyou Fan <shengyoufan@gmail.com>
2019-12-19 00:23:51 +0800

    建立 Task Model

---
+++ app/Model/Task.php
@@ -0,0 +1,37 @@
+<?php
+
+declare (strict_types=1);
+
+namespace App\Model;
+
+use Hyperf\DbConnection\Model\Model;
+/**
+ */
+class Task extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tasks';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'completed',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'title' => 'string',
+        'completed' => 'boolean',
+    ];
+}
