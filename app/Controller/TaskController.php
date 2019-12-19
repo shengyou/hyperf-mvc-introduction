6876089b5bf7ce1d9e1d73218d91fe15ea5ae0b8
Shengyou Fan <shengyoufan@gmail.com>
2019-12-19 17:00:01 +0800

    建立應用程式畫面

--- app/Controller/TaskController.php
+++ app/Controller/TaskController.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Hyperf\View\RenderInterface;
+
 class TaskController
 {
-    public function index()
+    public function index(RenderInterface $render)
     {
-        return 'Hello, controller';
+        return $render->render('index', []);
     }
 }
