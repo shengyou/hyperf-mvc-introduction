d3e17a1e89fcc1ae6d365507cc8dc5ccf40d3134
Shengyou Fan <shengyoufan@gmail.com>
2019-12-19 16:55:20 +0800

    建立 Controller 並回傳字串

---
+++ app/Controller/TaskController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+class TaskController
+{
+    public function index()
+    {
+        return 'Hello, controller';
+    }
+}
--- config/routes.php
+++ config/routes.php
@@ -2,8 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Controller\TaskController;
 use Hyperf\HttpServer\Router\Router;
 
-Router::get('/', function () {
-    return 'Hello, world';
-});
+Router::get('/', [TaskController::class, 'index']);
